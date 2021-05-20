94a4e5a8b34ed1169f4904fcca51d667f6e081cb
Lorenzo Scott <lscott1220@gmail.com>
2021-05-20 15:21:56 -0700

    Added Welcome to console output

--- src/main/java/com/lorenzoscott/perScholasFirstGit/Main.java
+++ src/main/java/com/lorenzoscott/perScholasFirstGit/Main.java
@@ -3,7 +3,7 @@ package com.lorenzoscott.perScholasFirstGit;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("Welcome!");
 
 	}
 
